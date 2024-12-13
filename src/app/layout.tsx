855be5ebdc455b0e9b3f174f2f2b50942ce27f7f
MaxLiebsch <liebsch@dipmaxexport.com>
2024-12-13 13:41:18 +0100

    fix: add plausible

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import clsx from 'clsx'
 import '@/styles/tailwind.css'
 import { type Metadata } from 'next'
 import Script from 'next/script'
+import PlausibleProvider from 'next-plausible'
 
 export const metadata: Metadata = {
   title: {
@@ -39,6 +40,12 @@ export default function RootLayout({
         lexend.variable,
       )}
     >
+       <head>
+        <PlausibleProvider
+          domain={process.env.NEXT_PUBLIC_DOMAIN!}
+          enabled={true}
+        />
+      </head>
       <body className="flex h-full flex-col">{children}</body>
       <Script src="/js/latest.js" />
     </html>
